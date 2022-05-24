a5eede9adbe12009d7f8519a92f0d179a8574346
VladimirRanski <ranski.vladimir@gmail.com>
2022-05-24 21:49:02 +0300

    убрал synksize в 3м задании

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,27 @@
 // console.log('из-за слабого железа - работаю на Node.js v13.14.0.');
 const fs = require('fs');
 const path = require('path');
+const {stat} = require('fs');
 
 let dir_path = path.join('03-files-in-folder/secret-folder');
 
 fs.readdir(dir_path, {withFileTypes: true}, function (err,files){
   if (err) throw new Error(err);
   for (let i = 0; i < files.length; i++) {
-    if (files[i].isFile()) {
-      let result = '';
-      let nameFile = '';
-      let typeFile = '';
-      let sizeFile = 0;
-      typeFile = path.extname(files[i].name).slice(1);
-      nameFile = (files[i].name).slice(0, (files[i].name).lastIndexOf('.'));
-      let dir_path_full = dir_path + '\\' + files[i].name;
-      sizeFile = fs.statSync(dir_path_full).size / 1024;
+    if (!files[i].isFile()) {
+      continue;
+    }
+    let result = '';
+    let nameFile = '';
+    let typeFile = '';
+    let sizeFile = 0;
+    typeFile = path.extname(files[i].name).slice(1);
+    nameFile = (files[i].name).slice(0, (files[i].name).lastIndexOf('.'));
+    let dir_path_full = dir_path + '\\' + files[i].name;
+    fs.stat(dir_path_full, function (err, stats) {
+      sizeFile = stats['size'] / 1024;
       result = nameFile + ' - ' + typeFile + ' - ' + sizeFile + 'kb';
       console.log(result);
-    }
+    });
   }
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,9 +7,9 @@ const path = require('path');
 
 const path_folder = path.join(__dirname, 'files');
 const path_folder_new = path.join(__dirname, 'files-copy');
-let folder_out = path_folder;
+// let folder_start = path_folder;
 let folder_in = path_folder_new;
-let folder_teil = '';
+let folder_tail = '';
 console.log(path_folder);
 console.log(path_folder_new);
 
@@ -18,25 +18,25 @@ console.log(path_folder_new);
 function myCopy (folder_out){
 
   fs.mkdir(folder_in, {recursive: true},  function (err){
-    if (err) throw console.log('не создал папку: ', err);
+    if (err) throw console.log(`не создал папку: ${folder_in}`, err);
     console.log('папка создана');
   });
 
   fs.readdir(folder_out, {withFileTypes: true}, function (err,files) {
     if (err) throw console.log('не прочитал папку: ', err);
     console.log(files);
+    //copy file
     for (let i = 0; i < files.length; i++) {
       if (files[i].isFile()) {
         console.log(files[i]);
-        folder_teil = '\\' + files[i].name;
-        let start = folder_out + folder_teil;
-        let finish = folder_in + folder_teil;
+        folder_tail = '\\' + files[i].name;
+        let start = folder_out + folder_tail;
+        let finish = folder_in + folder_tail;
         fs.copyFile(start, finish, function (err) {
           if (err) throw console.log('не скопировал файл: ', err);
 
           console.log('start - ', start);
           console.log('finish - ', finish);
-
           console.log('скопирован', files[i]);
 
         });
@@ -44,19 +44,25 @@ function myCopy (folder_out){
         continue;
       }
     }
+
+    //copy directory
+    folder_in = path_folder_new;
     for (let i = 0; i < files.length; i++) {
-      if (files[i].isDirectory()){
-        folder_out += folder_teil;
-        folder_in += folder_teil;
+      if (!files[i].isFile()){
+        // folder_out += folder_tail;
+        // folder_in += folder_tail;
         console.log('новый путь - ', folder_in);
         fs.mkdir(folder_in, {recursive: true},  function (err){
-          if (err) throw console.log('не создал папку: ', err);
+          if (err) throw console.log(`не создал папку: ${folder_in}`, err);
           console.log('папка создана', folder_in);
+          myCopy(folder_out += '\\' + files[i].name);
+          console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         });
-        myCopy(folder_out);
+
       }
     }
   });
+
 }
 
 myCopy (path_folder);
